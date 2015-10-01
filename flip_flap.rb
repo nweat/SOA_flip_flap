2e5ace87d43fe78ce94decdc80d5da2cdc5ba5f4
soa <soa@localhost.localdomain>
2015-10-02 00:39:12 +0800

    Rubocop tested flip_flap.rb

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,14 @@
+require 'yaml'
 require_relative 'tsv_buddy'
-
+# FlipFlap
 class FlipFlap
-  # Do NOT create an initialize method
+  include TsvBuddy
 
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
 end
